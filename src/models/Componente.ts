3b2b058f3ba51d1d5d0edb66de5f30848120fcce
Adrian <acamilobravo@uniguajira.edu.co>
2021-10-11 11:24:57 -0500

    Creé los campos de Componente

--- src/models/Componente.ts
+++ src/models/Componente.ts
@@ -1,7 +1,8 @@
-import {Model, DataTypes} from 'sequelize';
+//import sequelize, {Model, DataTypes} from 'sequelize';
+const Sequelize = require('sequelize');
 import { database } from '../database/db';
 
-export class Componente extends Model {
+export class Componente extends Sequelize.Model {
     public nombre!: string; 
     public especificacion!: string; 
     public cantidad!: number; 
@@ -14,3 +15,34 @@ export interface ComponenteI {
     cantidad: number; 
     precio: number; 
 }
+
+Componente.init (
+    {
+        nombre: {
+            type: Sequelize.DataTypes.STRING,
+            allowNull: false
+        },
+
+        especificacion: {
+            type: Sequelize.DataTypes.TEXT,
+            allowNull: false
+        },
+
+        cantidad: {
+            type: Sequelize.DataTypes.INTEGER,
+            allowNull: false
+        },
+
+        precio: {
+            type: Sequelize.DataTypes.FLOAT,
+            allowNull: false
+        },
+    },
+
+    {
+        tableName: "componente",
+        sequelize: database,
+        timestamps: true
+    }
+
+);
