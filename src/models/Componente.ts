c746ecff4569c65fc2f0f5b2f927e1848b7d20d6
Adrian <acamilobravo@uniguajira.edu.co>
2021-10-11 11:07:00 -0500

    Cree la clase Componente

--- src/models/Componente.ts
+++ src/models/Componente.ts
@@ -2,5 +2,8 @@ import {Model, DataTypes} from 'sequelize';
 import { database } from '../database/db';
 
 export class Componente extends Model {
-    
+    public nombre!: string; 
+    public especificacion!: string; 
+    public cantidad!: number; 
+    public precio!: number; 
 }
