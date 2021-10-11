434bcc32d41dce372f73bc6d73d8e25de380e96f
Adrian <acamilobravo@uniguajira.edu.co>
2021-10-11 11:51:03 -0500

    Creé la ruta para Componente

---
+++ src/routes/componente.ts
@@ -0,0 +1,10 @@
+import { request, Response, Application } from "express";
+import { ComponenteController } from "../controllers/componente.controller";
+
+export class ComponenteRoutes {
+    public componenteController: ComponenteController = new ComponenteController();
+    
+    public routes(app: Application): void {
+        app.route('/componentes').get(this.componenteController.index);
+    }
+}
