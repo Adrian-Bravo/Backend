eee14b6fd560fdd0132b59540f8cd34b24591e1c
GeraSuarez00 <gsuarezs@uniguajira.edu.co>
2021-10-12 17:46:46 -0500

    Creé el método index para la clase AparatoController y modifiqué Tipo_electrodomesticos

--- src/controllers/Aparato.controller.ts
+++ src/controllers/Aparato.controller.ts
@@ -1,2 +1,10 @@
 import { Request, Response } from "express";
 import { Aparato } from "../models/Aparato";
+
+export class AparatoController {
+    public index(req: Request, res: Response) {
+        Aparato.findAll({})
+            .then((aparatos: Array<Aparato>) => res.json(aparatos))
+            .catch((err: Error) => res.status(500).json(err));
+    }
+}
--- src/controllers/Tipo_electrodomesticos.controller.ts
+++ src/controllers/Tipo_electrodomesticos.controller.ts
@@ -4,7 +4,7 @@ import {Tipo_electrodomesticos} from "../models/Tipo_electrodomesticos";
 export class Tipo_electrodomesticosController {
     public index(req: Request, res: Response) {
         Tipo_electrodomesticos.findAll({})
-            .then((Tipo_electrodomesticos: Array<Tipo_electrodomesticos>) => res.json(Tipo_electrodomesticos))
+            .then((tipo_electrodomesticos: Array<Tipo_electrodomesticos>) => res.json(tipo_electrodomesticos))
             .catch((err: Error) => res.status(500).json(err));
     }
 }
