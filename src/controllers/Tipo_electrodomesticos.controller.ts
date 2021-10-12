f89b5d01b858a29bd07261ff65adace593cacdb7
GeraSuarez00 <gsuarezs@uniguajira.edu.co>
2021-10-12 17:30:09 -0500

    Creé el método index  y exporté la clase Tipo_ElectrodomesticosController

--- src/controllers/Tipo_electrodomesticos.controller.ts
+++ src/controllers/Tipo_electrodomesticos.controller.ts
@@ -1,2 +1,10 @@
 import { Request, Response } from "express";
 import {Tipo_electrodomesticos} from "../models/Tipo_electrodomesticos";
+
+export class Tipo_electrodomesticosController {
+    public index(req: Request, res: Response) {
+        Tipo_electrodomesticos.findAll({})
+            .then((Tipo_electrodomesticos: Array<Tipo_electrodomesticos>) => res.json(Tipo_electrodomesticos))
+            .catch((err: Error) => res.status(500).json(err));
+    }
+}
