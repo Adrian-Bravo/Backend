25ff4dff8ef3a081d66d98eac7af408775cd6a9e
GeraSuarez00 <gsuarezs@uniguajira.edu.co>
2021-10-20 15:00:00 -0500

    Creé las diferentes rutas para la clase Aparato

--- src/routes/Aparato.ts
+++ src/routes/Aparato.ts
@@ -6,6 +6,9 @@ export class AparatoRoutes {
     public aparatoController: AparatoController = new AparatoController();
     
     public routes(app: Application): void {
-        app.route('/aparatos').get(this.aparatoController.index);
-    }
+        app.route('/aparato').get(this.aparatoController.getAparato)
+        app.route('/aparato/:id').get(this.aparatoController.getOneAparato)
+        app.route('/aparato').post(this.aparatoController.creaAparato)
+        app.route('/aparato/:id').patch(this.aparatoController.updateAparato)
+          }
 }
