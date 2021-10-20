5bca40548ff151fae75504701062ba6b70975158
GeraSuarez00 <gsuarezs@uniguajira.edu.co>
2021-10-20 14:23:50 -0500

    cambié el puerto en el server

--- src/routes/Tipo_electrodomesticos.ts
+++ src/routes/Tipo_electrodomesticos.ts
@@ -6,6 +6,9 @@ export class Tipo_electrodomesticosRoutes {
     public tipo_electrodomesticosController: Tipo_electrodomesticosController = new Tipo_electrodomesticosController();
     
     public routes(app: Application): void {
-        app.route('/tipo_electrodomesticos').get(this.tipo_electrodomesticosController.index);
+        app.route('/tipo_electrodomesticos').get(this.tipo_electrodomesticosController.getTipo_electrodomesticos);
+        app.route('/tipo_electrodomesticos/:id').get(this.tipo_electrodomesticosController.getOneTipo_electrodomestico);
+        app.route('/tipo_electrodomestico').post(this.tipo_electrodomesticosController.creaTipo_electrodomesticos);
+        app.route('/tipo_electrodomestico/:id').patch(this.tipo_electrodomesticosController.updateTipo_electrodomesticos);
     }
 }
