5cc2a66e1f994c6a9778d16f3c37e84fb19f8341
Ivonne-cas <ivonneximenacaste@gmail.com>
2021-11-01 16:26:18 -0500

    corregi get

--- src/controllers/Fabricante.controller.ts
+++ src/controllers/Fabricante.controller.ts
@@ -3,55 +3,21 @@ import { Request,Response } from "express";
 import { Fabricante, FabricanteI } from "../models/Fabricante";
 
 
-export class FabricanteController{  
-    // MOSTRAR LO DE LA TABLA
+export class FabricanteController{ 
     public async getFabricante(req:Request, res:Response){
-    try {
-        const fabricante = await Fabricante.findAll()
-        res.status(200).json({fabricante})
-    } catch (error) {
-        res.status(500).json({msg: "OPERACION INVALIDA"})
-    }
-    }
-    //mostrar uno solo o buscar
-    public async getOneFabricante (req:Request, res:Response){
-        const {id}= req.params
-
         try {
-            const fabricante:FabricanteI | null =await Fabricante.findOne({where:{id:id}})
-            res.status(200).json({fabricante})
-        } catch (error) {
-            res.status(500).json({msg:"no se puede mostrar uno a uno"})
-        }
-    }
-    // INGRESAR DATOS
-    public async creaFabricante(req:Request, res:Response){
-        const body: FabricanteI =req.body
-        try {
-            const fabricante =await Fabricante.create(body)
-            res.status(200).json({fabricante})
-        } catch (error) {
-            res.status(500).json({msg: "OPERACION INVALIDA"})
-            
-        }
-
-    }
-    //actualizar
-    public async updateFabricante (req:Request, res:Response){
-        const {id}= req.params
-        const body : FabricanteI = req.body
-        try {
-            
-            const fabricante_existente: FabricanteI | null =await Fabricante.findByPk(id)
-            await Fabricante.update(
-                body,{where:{id:id}}
+            const fabricante = await Fabricante.findAll(//la variable user obtiene un registro de todos los usuarios que estan en el modelo User
+            {
+                where: {status:"Activado"}
+            }
             )
-            const fabricante: FabricanteI | null =await Fabricante.findByPk(id)
-            
-            res.status(200).json({fabricante})
-
-        } catch (error) {
-            res.status(500).json({msg:"no se pudo actualizar"})
+            if (!fabricante){ //if (users) si hay registros y (!users) no hay registros
+                res.status(400).json({msg:'No hay aparatos registrados'}) //Retorna la respuesta: errores 400 0 500 si no hay registros
+            }
+            return res.status(200).json({fabricante}) //Retorna usuarios
+        
+        }catch (error){
+            res.status(500).json({msg:'Error interno: No hay conexión con la base de datos'})
         }
     }
 
--- src/routes/Fabricante.ts
+++ src/routes/Fabricante.ts
@@ -5,9 +5,7 @@ export class FabricanteRoutes{
     public fabricanteController:FabricanteController = new FabricanteController();
     public routes(app: Application): void {
         app.route('/fabricante').get(this.fabricanteController.getFabricante)
-        app.route('/fabricante/:id').get(this.fabricanteController.getOneFabricante)
-        app.route('/fabricante').post(this.fabricanteController.creaFabricante)
-        app.route('/fabricante/:id').patch(this.fabricanteController.updateFabricante)
+        
         
     }
 }
