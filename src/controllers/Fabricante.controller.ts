543458ee7493763f31284dcd2bf487a84b8e42e7
ivonne ximena castellanos martinez <69778909+Ivonne-cas@users.noreply.github.com>
2021-11-23 09:35:24 -0500

    Update Fabricante.controller.ts
    modifique fabricantes y para poderlo visualizar en el frontend

--- src/controllers/Fabricante.controller.ts
+++ src/controllers/Fabricante.controller.ts
@@ -4,22 +4,23 @@ import { Fabricante, FabricanteI } from "../models/Fabricante";
 
 
 export class FabricanteController{ 
-    public async getFabricante(req:Request, res:Response){
-        try {
-            const fabricante = await Fabricante.findAll(//la variable user obtiene un registro de todos los usuarios que estan en el modelo User
-            {
-                where: {status:"Activido"}
-            }
-            )
-            if (!fabricante){ //if (users) si hay registros y (!users) no hay registros
-                res.status(400).json({msg:'No hay aparatos registrados'}) //Retorna la respuesta: errores 400 0 500 si no hay registros
-            }
-            return res.status(200).json({fabricante}) //Retorna usuarios
-        
-        }catch (error){
-            res.status(500).json({msg:'Error interno: No hay conexión con la base de datos'})
-        }
-    }
+    public getFabricante(req: Request, res: Response) {
+        Fabricante.findAll({})
+            .then((users: Array<Fabricante>) => res.json(users))
+            .catch((err: Error) => res.status(500).json(err));
+
+    } 
+    // public async getFabricante(req:Request, res:Response){
+    //     try {
+    //         const fabricante = await Fabricante.findAll(//la variable user obtiene un registro de todos los usuarios que estan en el modelo User
+    //         {
+    //             where: {status:"Activido"}
+    //         }
+    //         )
+    //         if (!fabricante){ //if (users) si hay registros y (!users) no hay registros
+    //             res.status(400).json({msg:'No hay aparatos registrados'}) //Retorna la respuesta: errores 400 0 500 si no hay registros
+    //         }
+    //         return res.status(200).json({fabricante}) //Retorna usuarios
     //mostrar uno solo o buscar
     public async getOneFabricante (req:Request, res:Response){
         const {id}= req.params
